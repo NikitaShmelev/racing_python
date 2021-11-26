1e7bbd241176372030ff7c5a8458202a8e7cff7f
n.shmelyov <Shmelev507@gmail.com>
2021-11-26 21:36:45 +0100

    send

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pygame
+from pygame.transform import rotate
 pygame.init() #initializes the Pygame
 from pygame.locals import* #import all modules from Pygame
-width = 1200
-height = 720
+
+width = 1680
+height = 1030
 screen = pygame.display.set_mode((width, height))
+over_font = pygame.font.Font('freesansbold.ttf', 64)
 
 #changing title of the game window
 pygame.display.set_caption('Racing Beast')
 
+def game_over_text():
+    over_text = over_font.render("Pojebało pana", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
 
 #defining our gameloop function
 def gameloop():
@@ -20,6 +26,7 @@ def gameloop():
     maincar = pygame.image.load('car_small.png')
     maincarX = 350
     maincarY = 495
+    rotate_down, rotate_up = False, True
     x_speed = 0
     y_speed = 0
 
@@ -41,10 +48,17 @@ def gameloop():
                     x_speed -= 0.5
                 
                 if event.key == pygame.K_UP:
+                    if rotate_down:
+                        maincar = pygame.transform.rotate(maincar, 180)
+                        rotate_down, rotate_up = False, True
                     y_speed -= 0.5
                     
                 if event.key == pygame.K_DOWN:
+                    if rotate_up:
+                        maincar = pygame.transform.rotate(maincar, 180)
+                        rotate_up, rotate_down = False, True
                     y_speed += 0.5
+
 
 
         #CHANGING COLOR WITH RGB VALUE, RGB = RED, GREEN, BLUE 
@@ -60,17 +74,22 @@ def gameloop():
         if maincarX <= -10:
             maincarX = -10
             x_speed = 0
+            game_over_text()
+            
         elif maincarX >= width - x_coords_fix:
             maincarX = width - x_coords_fix
             x_speed = 0
+            game_over_text()
         
         maincarY += y_speed
         if maincarY <= 0:
             maincarY = 0
             y_speed = 0
+            game_over_text()
         elif maincarY >= height - y_coords_fix:
             maincarY = height - y_coords_fix
             y_speed = 0
+            game_over_text()
 
         pygame.display.update()
 
