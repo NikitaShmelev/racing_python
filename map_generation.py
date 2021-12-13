3818c592a4d630396e747767a522aeb9e00d1020
n.shmelyov <shmelev507@gmail.com>
2021-12-13 22:06:15 +0100

    gen map update

--- map_generation.py
+++ map_generation.py
@@ -3,11 +3,12 @@ import numpy as np
 import random
 # shape = (45, 30)
 # map = np.zeros(shape, dtype=float, order='C')
-
+height = 10
+width = 10
 map = [
-    [0 for i in range(30)] for i in range(50)
+    [0 for i in range(height)] for i in range(width)
 ]
-start_pos_x = 9
+start_pos_x = int((len(map[0]) - 1)/2)
 start_pos_y = len(map) - 1
 width = 4
 
@@ -25,17 +26,42 @@ direction = 1 #random.randint(0, 1)
 rand_anle = 45 #random.randint(1, 89)
 # 0 - left, 1 - right
 if direction:
-    left_road_y = len(map) - int((start_pos_x + 1) * np.sin(round(rand_anle*np.pi/180)) / (np.sin(
+    y = len(map) - int((start_pos_x + 1) * np.sin(round(rand_anle*np.pi/180)) / (np.sin(
                     fabs(round(
                         np.pi/2 - rand_anle*np.pi/180
                     ))
                 )) - 1)
-map[left_road_y][0] = 1
 
+vektors = {0: {
+                'right_start': (right_road_x, right_road_y), # start of right road's side
+                'left_start' : (left_road_x, left_road_y) # start of left road's side
+                # ends of the roads will be added later - required len of dicts = 4
+                }
+            }
+while left_road_y != y:
+    if y < left_road_y - 1:
+        left_road_y -= 1
+        left_road_x -= 1
+        right_road_y -= 1
+        right_road_x -= 1
+        map[left_road_y][left_road_x] = 1 if map[left_road_y][left_road_x] != 1 else 0
+        map[right_road_y][right_road_x] = 1 if map[right_road_y][right_road_x] != 1 else 0
+    else:
+        
+        num_key = list(vektors.keys())[-1]
+        len_of_coords = len(vektors[num_key].keys())
+        if len_of_coords != 4:
+            vektors[num_key]['right_end'] = (right_road_x, right_road_y) # end
+            vektors[num_key]['left_end'] = (left_road_x, left_road_y) # end
+        else:
+            num_key += 1
+            vektors[num_key]['right_start'] = vektors[num_key-1]['right_end']
+            vektors[num_key]['left_start'] = vektors[num_key-1]['left_end']
+        break
 
 for line in map:
     print(line)
-print(start_pos_x, start_pos_y, width)
-
+for key in vektors.keys():
+    print(vektors[key])
 
 # print(y)
