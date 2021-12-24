527c2fc4b1a3a44d595f99cd0d3031fce5123cd3
n.shmelyov <shmelev507@gmail.com>
2021-12-24 20:56:43 +0100

    map update, bounce

--- main_2.py
+++ main_2.py
@@ -12,9 +12,14 @@ window = pygame.display.set_mode((width, height))
 TEXTCOLOR = (255, 255, 255)
 clock = pygame.time.Clock()
 MAIN_FONT = pygame.font.SysFont("comicsans", 44)
-
-TRACK_BORDER = scale_image(pygame.image.load("images/track-border.png"), 0.9)
-TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
+# TRACK_BORDER = scale_image(pygame.image.load("images/track-border.png"), 0.9)
+# TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
+images = [
+    # (img, (x, y))
+]
+images_masks = [
+    # (mask, (x, y))
+]
 
 class MainCar:
     def __init__(self, x_pos, y_pos, 
@@ -33,13 +38,11 @@ class MainCar:
     
 
     def draw(self, window):
-        # print(self.img, window)
         blit_rotate_center(
                     window, self.img, 
                     (self.x_pos, self.y_pos), 
                     self.angle
                     )
-        # pygame.display.update()
 
     def rotate(self, left=False, right=False):
         if left:
@@ -51,10 +54,8 @@ class MainCar:
         radians = math.radians(self.angle)
         vertical = math.cos(radians) * self.vel
         horizontal = math.sin(radians) * self.vel
-
         self.y_pos -= vertical
         self.x_pos -= horizontal
-        print(self.x_pos, self.y_pos)
 
     def move_forward(self):
         self.vel = min(self.vel + self.acceleration, self.max_vel)
@@ -77,8 +78,8 @@ class MainCar:
         
     def collide(self, mask, x=0, y=0):
         car_mask = pygame.mask.from_surface(self.img)
-        offset = (int(self.x - x), int(self.y - y))
-        poi = mask.overlap(car_mask, offset)
+        offset = (int(-x+self.x_pos + 50), int(-y+self.y_pos))
+        poi = car_mask.overlap(mask, offset)
         return poi
     
 
@@ -92,14 +93,21 @@ class MainCar:
         self.vel = -self.vel
         self.move()
 
-# def draw_car(main_car):
-#     draw_car
+
 
 def drawText(text, font, surface, x, y):
     textobj = font.render(text, 1, TEXTCOLOR)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
+
+def handle_collision(main_car):
+    for mask in images_masks:
+        print(mask[0].get_size())
+        if main_car.collide(mask=mask[0], x=mask[1], y=mask[2]) != None:
+            main_car.bounce()
+        
+            
 
 def move_player(player_car):
     keys = pygame.key.get_pressed()
@@ -120,20 +128,51 @@ def move_player(player_car):
     if not moved:
         player_car.reduce_speed()
 
+def draw_objects(images):
+    for img, pos in images:
+        window.blit(img, pos)
+
+
+def generate_map(main_car):
+    x_left = main_car.x_pos - 100
+    x_right = main_car.x_pos + 100
+    y = main_car.y_pos
+    for i in range(height//100):
+        left_side = scale_image(pygame.image.load("images/road_left.png"), 1)
+        left_side_mask = pygame.mask.from_surface(left_side)
+        
+        right_side = scale_image(pygame.image.load("images/road_right.png"), 1)
+        right_side_mask = pygame.mask.from_surface(right_side)
+        images_masks.append(
+            (left_side_mask, x_left, y)
+            )
+        images_masks.append(
+            (right_side_mask, x_right, y)
+            )
+        images.append(
+            (left_side, (x_left, y))
+        )
+        images.append(
+            (right_side, (x_right, y))
+        )
+        # x_right += 100
+        y -= left_side.get_height()
+        # break
+
 def gameloop():
     main_car = MainCar(
-        x_pos=width/2, y_pos=height/2,
+        x_pos=100, y_pos=height-100,
         max_vel=10, rotation_vel=4,
         image_path='images/car_images/car_small_up.png', 
         acceleration=0.2, start_vel=0,
         angle=0
         )
-
+    generate_map(main_car)
     while run:
         clock.tick(FPS)
 
         window.fill((0,0,0))
-        
+        draw_objects(images)
         main_car.draw(window)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -149,6 +188,10 @@ def gameloop():
         pygame.draw.line(
             window, 'red', (1029, 1679), (20, 20), width=1
         )
+        handle_collision(
+            main_car, 
+            # game_info
+            )
         pygame.display.update()
 
         
