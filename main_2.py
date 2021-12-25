66438bd6dad730545c30285279dda49252f119fb
n.shmelyov <shmelev507@gmail.com>
2021-12-25 15:18:07 +0100

    map update

--- main_2.py
+++ main_2.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 import math, sys
 from utils import scale_image, blit_rotate_center, blit_text_center
 pygame.font.init()
@@ -15,10 +16,11 @@ MAIN_FONT = pygame.font.SysFont("comicsans", 44)
 # TRACK_BORDER = scale_image(pygame.image.load("images/track-border.png"), 0.9)
 # TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 images = [
+    
     # (img, (x, y))
 ]
 images_masks = [
-    # (mask, (x, y))
+    # (mask, x, y)
 ]
 
 class MainCar:
@@ -76,9 +78,9 @@ class MainCar:
         self.vel = max(self.vel - self.acceleration / 2, 0)
         self.move()
         
-    def collide(self, mask, x=0, y=0):
+    def collide(self, mask, x=0, y=0, x_fix=50):
         car_mask = pygame.mask.from_surface(self.img)
-        offset = (int(-x+self.x_pos + 50), int(-y+self.y_pos))
+        offset = (int(-x+self.x_pos + x_fix), int(-y+self.y_pos))
         poi = car_mask.overlap(mask, offset)
         return poi
     
@@ -104,7 +106,8 @@ def drawText(text, font, surface, x, y):
 def handle_collision(main_car):
     for mask in images_masks:
         print(mask[0].get_size())
-        if main_car.collide(mask=mask[0], x=mask[1], y=mask[2]) != None:
+        x_fix = 0 if len(mask) == 4 else 50
+        if main_car.collide(mask=mask[0], x=mask[1], y=mask[2], x_fix=x_fix) != None:
             main_car.bounce()
         
             
@@ -132,31 +135,83 @@ def draw_objects(images):
     for img, pos in images:
         window.blit(img, pos)
 
+def generate_straight_road_y(x_left, x_right, y, direction):
+    left_side = scale_image(pygame.image.load("images/road/road_left.png"), 1)
+    left_side_mask = pygame.mask.from_surface(left_side)
+    right_side = scale_image(pygame.image.load("images/road/road_right.png"), 1)
+    right_side_mask = pygame.mask.from_surface(right_side)
+    images_masks.append((left_side_mask, x_left, y))
+    images_masks.append((right_side_mask, x_right, y))
+    images.append((left_side, (x_left, y)))
+    images.append((right_side, (x_right, y)))
+    y += direction*left_side.get_height()
+    return y
+
+
+def road_collision(road_image, x, y):
+    mask = pygame.mask.from_surface(road_image)
+    for i in images_masks:
+        image_x = i[1]
+        image_y = i[2]
+        offset = (int(-image_x+x + 50), int(-image_y+y))
+        img_mask = i[0]
+        poi = mask.overlap(img_mask, offset)
+        if poi != None:
+            return False
+    else:
+        return True
+
 
 def generate_map(main_car):
-    x_left = main_car.x_pos - 100
-    x_right = main_car.x_pos + 100
-    y = main_car.y_pos
-    for i in range(height//100):
-        left_side = scale_image(pygame.image.load("images/road_left.png"), 1)
-        left_side_mask = pygame.mask.from_surface(left_side)
+    x_left = main_car.x_pos - 50
+    x_right = main_car.x_pos + 90
+
+    road_turn = False
+    road_orientation = -1 # -1 = up(1=down) if road_turn == false else -1=left,1=right
+    #first step
+    y = generate_straight_road_y(
+        x_left, x_right, main_car.y_pos, road_orientation
+        # direction -1 = up; +1 = down
+    )
+    # y_left, y_right = generate_straight_road_x(main_car)
+    
+    y_left, y_right = y, y
+    
+
+    # while True:
+    
+    # if y > 0 and y < height:
+    direction = 1#random.randint(1,3)
+        # 1 - left, 2 - straight, 3 - right
+    if direction == 1:
+        left_side = scale_image(pygame.image.load("images/road/Turn_left_1.png"), 1)
+        if x_left - left_side.get_width() <= 2*left_side.get_width():
+            # no widht for turn left
+            # then turn right
+            right_side = scale_image(pygame.image.load("images/road/Turn_right_1.png"), 1)
+            if road_collision(right_side, x_right, y_right):
+                road_orientation = 1
+                road_turn = True
+
+                left_side_up = scale_image(pygame.image.load("images/road/road_left.png"), 1)
+                images.append((left_side_up, (x_left, y_left)))
+                images_masks.append((pygame.mask.from_surface(left_side_up), x_left, y_left))
+                y_left -= left_side_up.get_height()
+
+                left_side_turn = scale_image(pygame.image.load("images/road/Turn_right_1.png"), 1.4)
+                images.append((left_side_turn, (x_left, y_left)))
+                images_masks.append((pygame.mask.from_surface(left_side_turn), x_left, y_left, 0)) #50 - x fix
+                x_left += left_side_turn.get_width()
+                y_left -= left_side_turn.get_height()
+                
+            else:
+                print('huit')
+        else:
+            print('git add left turn')
+    # else:
+    #     break
         
-        right_side = scale_image(pygame.image.load("images/road_right.png"), 1)
-        right_side_mask = pygame.mask.from_surface(right_side)
-        images_masks.append(
-            (left_side_mask, x_left, y)
-            )
-        images_masks.append(
-            (right_side_mask, x_right, y)
-            )
-        images.append(
-            (left_side, (x_left, y))
-        )
-        images.append(
-            (right_side, (x_right, y))
-        )
-        # x_right += 100
-        y -= left_side.get_height()
+            
         # break
 
 def gameloop():
@@ -183,11 +238,10 @@ def gameloop():
         move_player(main_car)
         # over_text = over_font.render(f'{main_car.x_pos=} {main_car.y_pos=}', True, (255, 255, 255))
         
-        # window.blit(over_text, (200, 250))
         drawText(f'{round(main_car.x_pos)=} {round(main_car.y_pos)=}', MAIN_FONT, window, 128, 0)
-        pygame.draw.line(
-            window, 'red', (1029, 1679), (20, 20), width=1
-        )
+        # pygame.draw.line(
+        #     window, 'red', (1029, 1679), (20, 20), width=1
+        # )
         handle_collision(
             main_car, 
             # game_info
