b7789fb82384d3b4fc757f6bf301a0b777e633db
n.shmelyov <shmelev507@gmail.com>
2021-12-28 03:18:46 +0100

    left turn ater right turn

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import random
 import math, sys
 from map_generation import Map
+from car import MainCar
 from utils import scale_image, blit_rotate_center, blit_text_center
 pygame.font.init()
 over_font = pygame.font.Font('freesansbold.ttf', 26)
@@ -16,104 +17,24 @@ clock = pygame.time.Clock()
 MAIN_FONT = pygame.font.SysFont("comicsans", 44)
 
 
-class MainCar:
-    def __init__(self, x_pos, y_pos, 
-                max_vel, rotation_vel, 
-                image_path, acceleration, 
-                start_vel, angle):
-        self.img = scale_image(pygame.image.load(image_path), 0.55)
-        # self.img = pygame.image.load(image_path)
-        self.max_vel = max_vel
-        self.vel = start_vel
-        self.rotation_vel = rotation_vel
-        self.angle = angle
-        self.x_pos, self.y_pos = x_pos, y_pos
-        self.acceleration = acceleration
-
-    
-
-    def draw(self, window):
-        blit_rotate_center(
-                    window, self.img, 
-                    (self.x_pos, self.y_pos), 
-                    self.angle
-                    )
-
-    def rotate(self, left=False, right=False):
-        if left:
-            self.angle += self.rotation_vel
-        elif right:
-            self.angle -= self.rotation_vel
-
-    def move(self):
-        radians = math.radians(self.angle)
-        vertical = math.cos(radians) * self.vel
-        horizontal = math.sin(radians) * self.vel
-        self.y_pos -= vertical
-        self.x_pos -= horizontal
-
-    def move_forward(self):
-        self.vel = min(self.vel + self.acceleration, self.max_vel)
-        self.move()
-
-    def move_backward(self):
-        self.vel = max(self.vel - self.acceleration, -self.max_vel/2)
-        self.move()
-    
-    def update_path_point(self):
-        target = self.path[self.current_point]
-        rect = pygame.Rect(
-            self.x_pos, self.y_pos, self.img.get_width(), self.img.get_height())
-        if rect.collidepoint(*target):
-            self.current_point += 1
-
-    def reduce_speed(self):
-        self.vel = max(self.vel - self.acceleration / 2, 0)
-        self.move()
-        
-    def collide(self, mask, x=0, y=0, x_fix=0, y_fix=0):
-        car_mask = pygame.mask.from_surface(self.img)
-        offset = (int(-x+self.x_pos + x_fix), int(-y+self.y_pos+y_fix))
-        poi = car_mask.overlap(mask, offset)
-        return poi
-    
-
-    def reset(self):
-        self.x, self.y = self.START_POS
-        self.angle = 0
-        self.vel = 0
-
-
-    def bounce(self):
-        self.vel = -self.vel
-        self.move()
-
-
-
 def drawText(text, font, surface, x, y):
     textobj = font.render(text, 1, TEXTCOLOR)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
+
 def handle_collision(main_car, images_masks):
     for mask in images_masks:
-        #0 50
         x_fix = mask[3]
-        # 100 0
         y_fix = mask[4]
-        # y_fix = -10 if len(mask) == 4 else 0
-
         if main_car.collide(mask=mask[0], x=mask[1], y=mask[2], x_fix=x_fix, y_fix=y_fix) != None:
             main_car.bounce()
-        
-            
+
 
 def move_player(player_car):
     keys = pygame.key.get_pressed()
     moved = False
-
-
     if keys[pygame.K_a]:
         player_car.rotate(left=True)
     if keys[pygame.K_d]:
@@ -124,93 +45,60 @@ def move_player(player_car):
     if keys[pygame.K_s]:
         moved = True
         player_car.move_backward()
-
     if not moved:
         player_car.reduce_speed()
+
 
 def draw_objects(images):
     for img, pos in images:
         window.blit(img, pos)
 
 
-
-
-
-
-
-
-# def road_up_turn_right(x_right, y_right, x_left, y_left, road_orientation):
-#     # right_side = scale_image(pygame.image.load("images/road/Turn_right_1.png"), 1)
-#     # if road_collision(right_side, x_right, y_right):
-        
-#     y_left = generate_straight_road_y(x_left, y_left, road_orientation, 50, 0)
-#     y_left = generate_straight_road_y(x_left, y_left, road_orientation, 50, 0)
-#     y_right = generate_straight_road_y(x_right, y_right, road_orientation, 45, -60)
-#     corner_left = scale_image(pygame.image.load("images/road/Road_turn_corner_5.png"), 1)
-#     images.append((corner_left, (x_left-corner_left.get_width()+13, y_left+35)))
-#     x_left += 10
-#     y_left += 25 
-#     y_left += images[-1][0].get_height()
-#     road_orientation = 1
-#     x_left = generate_straight_road_x(x_left, y_left , road_orientation, -50, 100)
-#     x_left = generate_straight_road_x(x_left, y_left, road_orientation, -50, 100)
-#     corner_right = scale_image(pygame.image.load("images/road/Road_turn_corner_5.png"), 1)
-#     images.append(
-#         (corner_right, (
-#             x_right-corner_right.get_width()+13, y_right+37
-#             ))
-#         )
-#     x_right += 15
-#     y_right += 87+37
-#     x_right = generate_straight_road_x(x_right, y_right, road_orientation, -70, 50)
-#     return x_right, y_right, x_left, y_left, road_orientation
-
-
 def generate_map(main_car, map):
     map.step_straight_y()
     # map.step_straight_y()
-    
-   
 
-  
     # while True:
     
     # if y > 0 and y < height:
-    direction = 2#random.randint(1,3)
+    direction = 3#random.randint(1,3)
     
         # 1 - left, 2 - straight, 3 - right
     if direction == 3:
         if map.check_turn(width, height, True, -1):
-            map.horizontal_turn()
+            if map.road_turn:
+                map.horizontal_turn()
+            else:
+                map.step_straight_y()
         else:
             print('nie git')
+        
     elif direction == 1:
         if map.check_turn(width, height, True, 1):
-            map.horizontal_turn()
+            if map.road_turn:
+                map.horizontal_turn()
+            else:
+                map.step_straight_y()
         else:
             print('nie git')
     else:
+
         map.step_straight_y()
             # change direction 
-            
-        # left_side = scale_image(pygame.image.load("images/road/Turn_left_1.png"), 1)
-        # if x_left - left_side.get_width() <= 2*left_side.get_width():
-        #     # no widht for turn left
-        #     # then turn right
-        #     right_side = scale_image(pygame.image.load("images/road/Turn_right_1.png"), 1)
-        #     if road_collision(right_side, x_right, y_right):
-                
-        #         road_up_turn_right(x_right, y_right, x_left, y_left, road_orientation)
 
-        #     else:
-        #         print('huit')
-        # else:
-        #     print('git add left turn')
-    # else:
-    #     break
-        
-            
-        # break
+
+    ### turn left after right turn 
+    direction = 1
+    if direction == 1:
+        if map.check_turn(width, height, True, 1):
+            if map.road_turn:
+                map.horizontal_turn()
+            else:
+                map.step_straight_y(left=False, right=False)
+
+        else:
+            print('nie git')
+
 
 def gameloop():
     # while True:
@@ -221,7 +109,7 @@ def gameloop():
     #         break
     #     else:
     #         continue 
-    x_pos = 1450 #- 140
+    x_pos = 1450 - 140*3
     y_pos = height-100
     main_car = MainCar(
         x_pos=x_pos, y_pos= y_pos,
@@ -253,8 +141,7 @@ def gameloop():
 
             # if event.type == pygame.KEYDOWN:
         move_player(main_car)
-        # over_text = over_font.render(f'{main_car.x_pos=} {main_car.y_pos=}', True, (255, 255, 255))
-        
+
         drawText(f'{round(main_car.x_pos)=} {round(main_car.y_pos)=}', MAIN_FONT, window, 128, 0)
         # pygame.draw.line(
         #     window, 'red', (1029, 1679), (20, 20), width=1
--- map_generation.py
+++ map_generation.py
@@ -16,26 +16,53 @@ class Map:
         self.left_side_up_img = scale_image(pygame.image.load('images/bumpers/Road_14x14_left_side.png'), 1)
         self.bottom_image = scale_image(pygame.image.load('images/bumpers/Road_2x14_bottom_bumper.png'), 1)
         self.top_image = scale_image(pygame.image.load('images/bumpers/Road_2x14_top_bumper.png'), 1)
+    
     def draw_map(self, window):
         for img, pos in self.images:
             window.blit(img, pos)
 
-    def step_straight_y(self, left=True, right=True):
-        # road up 
-        if left:
-            self.y_left = self.generate_straight_road_y(
-                                self.x_left, self.y_left, self.road_orientation, 
-                                60, -80, side_img=self.left_side_up_img
-                                )
-        # młody fix image size
-        if right:          
-            młody_fix = -self.right_side_up_img.get_width()
+    def step_straight_y(self, left=True, right=True, test=False):
+        # road up
+        
+        if self.y_right == self.y_left:
+            if left:
+                self.y_left = self.generate_straight_road_y(
+                                    self.x_left, self.y_left, self.road_orientation, 
+                                    60, -80, side_img=self.left_side_up_img
+                                    )
+            # młody fix image size
+            if right:          
+                młody_fix = -self.right_side_up_img.get_width()
 
-            self.y_right = self.generate_straight_road_y(
-                                self.x_right + młody_fix, self.y_right, self.road_orientation, 
-                                -220, -80, side_img=self.right_side_up_img
-                                )
-    
+                self.y_right = self.generate_straight_road_y(
+                                    self.x_right + młody_fix, self.y_right, self.road_orientation, 
+                                    -220, -80, side_img=self.right_side_up_img
+                                    )
+        else:
+            if self.y_left < self.y_right:
+                self.horizontal_turn(orientation_fix=-1)
+                self.y_left += -self.left_side_up_img.get_height()
+                self.y_right += -self.left_side_up_img.get_height()
+                self.y_left = self.generate_straight_road_y(
+                                    self.x_left, self.y_left, self.road_orientation, 
+                                   85, -50, side_img=self.left_side_up_img
+                                    )
+                self.x_right += -self.top_image.get_width()
+                self.y_right = self.generate_straight_road_y(
+                                    self.x_right + 0, self.y_right, self.road_orientation, 
+                                    -220, -80, side_img=self.right_side_up_img
+                                    )
+                self.y_right = self.generate_straight_road_y(
+                                    self.x_right + 0, self.y_right, self.road_orientation, 
+                                    -220, -80, side_img=self.right_side_up_img
+                                    )
+                print(self.y_right, self.y_left)
+
+            else:
+                # CHECK THIS SHIT 
+                self.horizontal_turn(orientation_fix=1)
+ 
+
     def generate_straight_road_y(self, x, y, direction, x_fix, y_fix, side_img):
         side_mask = pygame.mask.from_surface(side_img)
         self.images_masks.append((side_mask, x, y, x_fix, y_fix))
@@ -46,7 +73,10 @@ class Map:
     def __change_direction__(self, road_turn, n):
         if self.road_turn == road_turn:
             self.road_turn = False if road_turn else True
-            self.road_orientation *= n
+            if self.y_left == self.y_right:
+                self.road_orientation *= n
+            else:
+                self.road_orientation *= -n
         else:
             self.road_turn = road_turn
             self.road_orientation *= n
@@ -61,15 +91,22 @@ class Map:
                 self.road_orientation = -1
                 self.changed = True
                 return True
-
+            else:
+                return False
         elif self.road_turn and self.road_orientation == -1:
             if self.x_right - self.right_side_up_img.get_width()*2 >= 0:
                 if self.y_left > -100:
                     self.changed = True
                     return True
         elif not self.road_turn and self.road_orientation == -1:
-            # up
-            pass
+            img_h = self.left_side_up_img.get_height()
+            if (self.y_right - img_h and self.y_right - img_h) > -100:
+                return True
+            elif (self.y_right + img_h and self.y_right + img_h) < height:
+                self.road_orientation = 1
+                return True
+            else:
+                return False
         elif not self.road_turn and self.road_orientation == 1:
             # down
             pass
@@ -83,6 +120,7 @@ class Map:
             if self.__road_collision__(
                                 self.bottom_image,
                                 self.x_right, self.y_right):
+                
                 return True
         else:
             pass
@@ -97,7 +135,7 @@ class Map:
         return x
 
     def step_straight_x(self, left=True, right=True, 
-                        y_fix=False, x_fix=False):
+                        y_fix=False, x_fix=False, orientation_fix=1):
         '''x_fix = {x_right: some, x_left: some}'''
         '''y_fix = {y_right: some, y_left: some}'''
         if x_fix:
@@ -115,22 +153,20 @@ class Map:
                 y_col_fix = 50
             self.x_right = self.__generate_straight_road_x__(
                                                 self.x_right, self.y_right, 
-                                                self.road_orientation,
+                                                self.road_orientation*orientation_fix,
                                                 x_col_fix, y_col_fix, self.bottom_image)
         if left:
-            print(self.changed)
             if self.changed:
                 x_col_fix = -70
                 y_col_fix = 60
             else:
-                x_col_fix = -60
+                x_col_fix = -50
                 y_col_fix = 100
             self.x_left = self.__generate_straight_road_x__(
                                                 self.x_left, self.y_left, 
-                                                self.road_orientation,
+                                                self.road_orientation*orientation_fix,
                                                 x_col_fix, y_col_fix, self.top_image)
-
-    def horizontal_turn(self):
+    def horizontal_turn(self, orientation_fix=1):
         if self.road_turn and self.road_orientation == 1:
             #right
             self.step_straight_y(right=False) # left side up\
@@ -142,7 +178,7 @@ class Map:
             self.step_straight_x()
             self.changed = False
         elif self.road_turn and self.road_orientation == -1:
-            self.step_straight_y(left=False)
+            #left
             y_fix={
                 'y_left': self.images[-1][0].get_height(), 
                 'y_right': self.images[-1][0].get_height()
@@ -153,8 +189,11 @@ class Map:
             }
             self.step_straight_x(left=False, x_fix=x_fix, y_fix=y_fix)
             self.step_straight_x()
-            self.changed = False
             
+            self.changed = False
+        elif not self.road_turn and self.road_orientation == -1:
+            self.step_straight_x(left=False, orientation_fix=orientation_fix)
+
             
        
 # def generate_straight_road_x(x, y, road_orientation, x_fix, y_fix):
